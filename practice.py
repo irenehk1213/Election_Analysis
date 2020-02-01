07bb2aa6a2c2fb66a43c85178084acc2acf6bbb2
irenehk1213 <59981299+irenehk1213@users.noreply.github.com>
2020-01-31 17:16:20 -0800

    Delete practice.py

--- practice.py
+++
@@ -1,6 +0,0 @@
-counties_dict={"Arapahoe": "422829", "Denver:"463353", "Jefferson": "434343" }
-message_to_candidate=(
-    f"You received {candidate_votes:,} number of votes."
-    f"The total number of votes in the election was {total_votes:,}"
-    f"You received {candidate_votes/total_votes*100: .2f}% of the total votes"
-)
