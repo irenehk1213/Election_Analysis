8f5c883bfd38edb4387a3fdaac93088b0287ac82
Irene Kang <irenehk@Irenes-MacBook-Pro-2.local>
2020-02-01 18:39:35 -0800

    Adding the election analysis.

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -126,11 +126,11 @@ with open(file_to_save, "w") as txt_file:
     print(winning_county_summary)
 
     winning_candidate_summary = (
-        f"----------------------------\n"
+        f"—————————————\n"
         f"Winner: {winning_candidate}\n"
         f"Winning Vote Count: {winning_count:,}\n"
         f"Winning Percentage: {winning_percentage:.1f}%\n"
-        f"----------------------------\n")
+        f"—————————————\n")
 
     print(winning_candidate_summary)
 
